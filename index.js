216bec9aafdc95211dcfcb848918ad0cfd8c9150
Nicole George <hello@nicolegeorge.me>
2020-04-27 13:59:49 +0930

    added markdown for the readme file. added 2 badges - github and portfolio link.

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const fs = require('fs');
+const util = require('util');
 
-inquirer
-  .prompt([
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// script outline from:  https://www.npmjs.com/package/inquirer
+
+function promptUser() {
+    return inquirer.prompt([
       { type: "input",
-      message: "What is your GitHub username?",
+      message: ":nerd_face: What is your GitHub username?",
       name: "userName",
       default: "adorkablynicole"
       },
-
-      {type: "input",
-      message: "what is your "
-
+      { type: "input",
+      message: ":e-mail: What is your GitHub email",
+      name: "userEmail"
       },
       { type: "input", 
-      message: "What is your project title?", 
+      message: ":open_file_folder: What is your project title?", 
       name: "projectTitle" 
       },
       { type: "input",
-      message: "Project Description",
+      message: ":spiral_notepad: Project Description",
       name: "projectDescription"
       },
-      { type: "list",
-      message: "Outline your table of contents",
-      name: "contents"
+      {type: "input",
+      message: ":electric_plug: Outline steps to install this project",
+      name: "projectInstallation"
       },
       { type: "input",
-      message: "outline how this appliation is used.",
-      name: "usage"
+      message: ":floppy_disk: outline how this appliation is used.",
+      name: "projectUsage"
       },
       { type: "list",
-      message: "What license does this app fall under?",
+      message: ":closed_lock_with_key: What license does this app fall under?",
       name: "license",
       default: "ISC",
       choices: [
@@ -40,33 +44,83 @@ inquirer
         "GNU"
       ]},
       {type: "input",
-      message: "how would you like people to contribute to your project?",
-      name: "contribute"
+      message: ":pager: how would you like people to contribute to your project?",
+      name: "projectContributors"
       },
       {type: "input",
-      message: "what was the testing process for this project?",
-      name: "testing"
+      message: ":gear: what was the testing process for this project?",
+      name: "projectTests"
       },
       {type: "input",
-      message: "Outline any further developments that need to be made.",
+      message: ":8ball: Submit questions about further development recommendations",
       name: "questions"
-      },
+      }
+    ]);
+   }
 
-])
+   function createReadMeFile(answers) {
+       return `
+       
+       # Project Title: 
+       ## ${data.projectTitle}
 
-// script outline from:  https://www.npmjs.com/package/inquirer
+       # Project Description:
+       ${data.projectDescription}
 
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
+        #Table of Contents:
+        1. Installation
+        2. Usage
+        3. License
 
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
+        ## Installation:
+            ${data.projectInstallation}
+
+        ## Usage:
+            ${data.projectUsage}
+
+        ## Contributors
+            ${data.projectContributors}
+
+        ## Testing:
+            ${data.projectTests}
+
+        # [!follow me on GitHub](![GitHub followers](https://img.shields.io/github/followers/NicoleGeorge?label=Follow%20me%20on%20GitHub&logoColor=%23FF4901&style=social))
+        # [![Check out my Work !](https://img.shields.io/badge/View%20Portfolio-check%20it%20out-%23ffd1d4)](https://${data.userName}.github.io/my-portfolio/)
+        `
+        }
+        
+        async function init() {
+          console.log("hi")
+          try {
+            const data = await promptUser();
+        
+            const text = generatereadme(data);
+        
+            await writeFileAsync("readme.md", text);
+        
+            console.log("Successfully wrote to index.html");
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        
+        init();
+        
+        `
+   }
+
+
+//   .then((answers) => {
+//     console.log(answers);
+//   })
+
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else when wrong
+//     }
+//   });
 
 const questions = [];
 
